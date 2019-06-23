a9e0bb67da3fcd6d094fa9e56c682097d9ebd388
Blessing Abobo <suspieabobo@yahoo.com>
2019-06-23 12:57:25 +0100

    ft(user): Implement user can signin
    -  Write tests for endpoint
    -  Create POST route to api/v1/auth/signin
    -  Use postman in testing endpoint
    
    [Delivers #166857450]

--- Server/middlewares/auth.js
+++ Server/middlewares/auth.js
@@ -21,20 +21,22 @@ export default class Auth {
       phoneNumber,
       address,
     } = req.body;
-    if (validator.isEmpty(firstName)) return sendErrorResponse(res, 400, 'FirstName cannot be empty');
-    if (validator.isEmpty(lastName)) return sendErrorResponse(res, 400, 'LastName cannot be empty');
-    if (validator.isEmpty(email)) return sendErrorResponse(res, 400, 'Email cannot be empty');
-    if (validator.isEmpty(password)) return sendErrorResponse(res, 400, 'Password cannot be empty');
-    if (validator.isEmpty(phoneNumber)) return sendErrorResponse(res, 400, 'Phone Number cannot be empty');
-    if (validator.isEmpty(address)) return sendErrorResponse(res, 400, 'Address cannot be empty');
-    if (!validator.isName(firstName)) return sendErrorResponse(res, 400, 'FirstName should contain only alphabets');
-    if (!validator.isName(lastName)) return sendErrorResponse(res, 400, 'LastName should contain only alphabets');
+    if (!validator.isName(firstName)) return sendErrorResponse(res, 400, 'FirstName is not allowed to be empty, and it should contain only alphabets');
+    if (!validator.isName(lastName)) return sendErrorResponse(res, 400, 'LastName is not allowed to be empty, and it should contain only alphabets');
     if (!validator.isLength(password, 6)) return sendErrorResponse(res, 400, 'Password should be at least six characters');
     if (!validator.isPassword(password)) return sendErrorResponse(res, 400, 'Password should contain at least one Uppercase letter, one lowercase letter, and at least one digit');
-    if (!validator.isEmail(email)) return sendErrorResponse(res, 400, 'Email is not valid');
+    if (!validator.isEmail(email)) return sendErrorResponse(res, 400, 'Please enter a valid email');
     if (!validator.isLength(phoneNumber, 11)) return sendErrorResponse(res, 400, 'Phone Number should be at least 11 characters');
     if (!validator.isNumber(phoneNumber)) return sendErrorResponse(res, 400, 'Your Phone Number is not valid');
-    if (!validator.isAddress(address)) return sendErrorResponse(res, 400, 'Your address is not vaild');
+    if (!validator.isAddress(address)) return sendErrorResponse(res, 400, 'Please enter a valid address');
+    return next();
+  }
+
+  static signin(req, res, next) {
+    const { email, password } = req.body;
+    if (!validator.isEmail(email)) return sendErrorResponse(res, 400, 'Please enter a valid email');
+    if (!validator.isLength(password, 6)) return sendErrorResponse(res, 400, 'Password should be at least six characters');
+    if (!validator.isPassword(password)) return sendErrorResponse(res, 400, 'Password should contain at least one Uppercase letter, one lowercase letter, and at least one digit');
     return next();
   }
 }
