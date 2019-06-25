4ebad8a6c6cc8b69b096537b9a302c6cbdd7aed8
Blessing Abobo <suspieabobo@yahoo.com>
2019-06-25 18:29:14 +0100

    Clean up master branch

--- UI/js/signup.js
+++
@@ -1,7 +0,0 @@
-const registerBtn = document.querySelector("#registerBtn");
-
-registerBtn.addEventListener("click", e => {
-    e.preventDefault();
-    window.location.replace("searchproperty.html");
-  });
-
